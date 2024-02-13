fffb8a206d4ad6e9154da974ac7b36999a67e655
jorgeoj <jorge2002oj@gmail.com>
2024-02-13 12:25:21 +0100

    Initial commit

---
+++ src/main/java/com/example/apirestrestaurantes/ApiRestRestaurantesApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirestrestaurantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestRestaurantesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestRestaurantesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apirestrestaurantes/ApiRestRestaurantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestrestaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestRestaurantesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
